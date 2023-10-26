2b700697cbe381407be139dbd50879881b453af0
TVHieu99 <hieutrantran102@gmail.com>
2023-10-26 11:12:38 +0700

    fix

--- js-shared/search.js
+++ js-shared/search.js
@@ -1,19 +1,28 @@
-
   // Lấy danh sách tất cả sản phẩm
   const allProducts = document.querySelectorAll('.sp');
+  const searchMessage = document.getElementById('searchMessage');
 
   // Lắng nghe sự kiện khi người dùng nhập từ khóa tìm kiếm
   function filterProducts() {
     const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    let foundMatch = false;
 
     allProducts.forEach(product => {
       const productName = product.querySelector('.tensp').textContent.toLowerCase();
 
       if (productName.includes(searchTerm)) {
         product.style.display = 'block'; // Hiển thị sản phẩm nếu tên sản phẩm khớp với từ khóa tìm kiếm
+        foundMatch = true;
       } else {
         product.style.display = 'none'; // Ẩn sản phẩm nếu tên sản phẩm không khớp với từ khóa tìm kiếm
       }
     });
-    document.getElementById('searchInput').value = '';
+
+    if (foundMatch) {
+      searchMessage.style.display = 'none'; // Ẩn thông báo nếu có sản phẩm khớp
+    } else {
+      searchMessage.style.display = 'block'; // Hiển thị thông báo nếu không có sản phẩm khớp
+    }
   }
+
+
