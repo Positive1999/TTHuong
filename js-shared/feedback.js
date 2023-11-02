ed3e40d44ceaff656b687aa43d2e536b7bc3f875
TVHieu99 <hieutrantran102@gmail.com>
2023-11-03 00:34:35 +0700

    add

--- js-shared/feedback.js
+++ js-shared/feedback.js
@@ -1,17 +1,25 @@
-// phan hoi 
 function Feedback(Feedback) {
     var shopEmail = "hieutran11525@gmail.com";
     var subject = "-----Phản hồi của khách hàng-----";
     var NameCustomer = Feedback.querySelector('#name-input').value;
     var Messenge = Feedback.querySelector('#message-input').value;
 
-    var body = "*******************************"+"\nTên khách hàng : " + NameCustomer +"\n Nội dung phản hồi :"+ Messenge;
+    if (NameCustomer === '' || Messenge === '') {
+        // Hiển thị hộp thoại SweetAlert2
+        Swal.fire({
+            icon: 'error',
+            title: 'Lỗi',
+            text: 'Vui lòng nhập đầy đủ thông tin trước khi gửi.',
+            confirmButtonText: 'OK'
+        });
+    } else {
+        var body = "*******************************" + "\nTên khách hàng : " + NameCustomer + "\nNội dung phản hồi :" + Messenge;
 
-    var mailtoLink = "mailto:" + shopEmail + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
-    window.location.href = mailtoLink;
+        var mailtoLink = "mailto:" + shopEmail + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+        window.location.href = mailtoLink;
 
-    // Đặt giá trị rỗng cho các trường nhập liệu
-    Feedback.querySelector('#name-input').value = '';
-    Feedback.querySelector('#message-input').value = '';
-
+        // Đặt giá trị rỗng cho các trường nhập liệu
+        Feedback.querySelector('#name-input').value = '';
+        Feedback.querySelector('#message-input').value = '';
+    }
 }
