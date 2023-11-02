1e2c7511be795cd258d9b2320610fe63822d5e5c
TVHieu99 <hieutrantran102@gmail.com>
2023-11-03 00:23:38 +0700

    add

--- js-shared/feedback.js
+++ js-shared/feedback.js
@@ -5,8 +5,13 @@ function Feedback(Feedback) {
     var NameCustomer = Feedback.querySelector('#name-input').value;
     var Messenge = Feedback.querySelector('#message-input').value;
 
-    var body = "*******************************"+"\nTên khách hàng : " + NameCustomer +"\n Nội dung :"+ Messenge;
+    var body = "*******************************"+"\nTên khách hàng : " + NameCustomer +"\n Nội dung phản hồi :"+ Messenge;
 
     var mailtoLink = "mailto:" + shopEmail + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
     window.location.href = mailtoLink;
+
+    // Đặt giá trị rỗng cho các trường nhập liệu
+    Feedback.querySelector('#name-input').value = '';
+    Feedback.querySelector('#message-input').value = '';
+
 }
